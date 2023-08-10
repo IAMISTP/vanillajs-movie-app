bf9c842b3b21372b427b5b7d56e91798b873e5ec
박혜민 <hyeminpak@gmail.com>
2023-08-11 03:41:04 +0900

    "test: 컴포넌트 안에서 배열에 조건을 넣은 결과 반복하여 표출 연습"

--- dist/index.f5c48570.js
+++ dist/index.f5c48570.js
@@ -581,31 +581,81 @@ const root = document.querySelector("#root");
 root.append(new (0, _appDefault.default)().el);
 
 },{"./App":"2kQhy","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"2kQhy":[function(require,module,exports) {
-// export default class App{
-//     constructor() {
-//         this.el = document.createElement('div');
-//         this.el.textContent = 'hello world'
-//     }
-// }
-// import { Component } from "./core/heropy";
-// export default class App extends Component {
-//   constructor(props) {
-//     super({
-//       tagName: "h1",
-//     });
-//   }
-//   render() {
-//     this.el.textContent = "Hello world!!";
-//   }
-// }
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 var _heropy = require("./core/heropy");
 class App extends (0, _heropy.Component) {
-    render() {
-        this.el.textContent = "Hello world!!";
+    constructor(){
+        super({
+            state: {
+                fruits: [
+                    {
+                        name: "Apple",
+                        price: 1000
+                    },
+                    {
+                        name: "Banana",
+                        price: 2000
+                    },
+                    {
+                        name: "Cherry",
+                        price: 3000
+                    }
+                ]
+            }
+        });
     }
-}
+    render() {
+        console.log(this.state.fruits);
+        this.el.innerHTML = /*html*/ `
+        <h1>Fruits</h1>
+        <ul>
+            ${this.state.fruits.filter((fruit)=>fruit.price < 3000).map((fruit)=>`<li>${fruit.name}</li>`).join("")}
+        </ul>
+    `;
+    }
+} // export default class App{
+ //     constructor() {
+ //         this.el = document.createElement('div');
+ //         this.el.textContent = 'hello world'
+ //     }
+ // }
+ // import { Component } from "./core/heropy";
+ // export default class App extends Component {
+ //   constructor(props) {
+ //     super({
+ //       tagName: "h1",
+ //     });
+ //   }
+ //   render() {
+ //     this.el.textContent = "Hello world!!";
+ //   }
+ // }
+ // import { Component } from "./core/heropy";
+ // export default class App extends Component {
+ //   constructor() {
+ //     super({
+ //       state: {
+ //         inputText: "",
+ //       },
+ //     });
+ //   }
+ //   render() {
+ //     this.el.classList.add("search");
+ //     this.el.innerHTML = /*html*/ `
+ //     <input />
+ //     <button>Click</button>
+ //     `;
+ //     const inputEl = this.el.querySelector("input");
+ //     inputEl.oninput = () => {
+ //       this.state.inputText = inputEl.value;
+ //     };
+ //     const buttonEl = this.el.querySelector("button");
+ //     buttonEl.addEventListener("click", () => {
+ //       console.log(this.state.inputText );
+ //     });
+ //   }
+ // }
 exports.default = App;
 
 },{"./core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"57bZf":[function(require,module,exports) {
@@ -615,8 +665,9 @@ parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "Component", ()=>Component);
 class Component {
     constructor(payload = {}){
-        const { tagName = "div" } = payload;
+        const { tagName = "div", state = {} } = payload;
         this.el = document.createElement(tagName);
+        this.state = state;
         this.render();
     }
     render() {}
--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
+import { Component } from "./core/heropy";
+
+export default class App extends Component {
+  constructor() {
+    super({
+      state: {
+        fruits: [
+          { name: "Apple", price: 1000 },
+          { name: "Banana", price: 2000 },
+          { name: "Cherry", price: 3000 },
+        ],
+      },
+    });
+  }
+  render() {
+    console.log(this.state.fruits);
+
+    this.el.innerHTML = /*html*/ `
+        <h1>Fruits</h1>
+        <ul>
+            ${this.state.fruits
+              .filter((fruit) => fruit.price < 3000)
+              .map((fruit) => `<li>${fruit.name}</li>`)
+              .join("")}
+        </ul>
+    `;
+  }
+}
 // export default class App{
 //     constructor() {
 //         this.el = document.createElement('div');
@@ -19,10 +47,31 @@
 //     this.el.textContent = "Hello world!!";
 //   }
 // }
-import { Component } from "./core/heropy";
+// import { Component } from "./core/heropy";
 
-export default class App extends Component {
-  render() {
-    this.el.textContent = "Hello world!!";
-  }
-}
+// export default class App extends Component {
+//   constructor() {
+//     super({
+//       state: {
+//         inputText: "",
+//       },
+//     });
+//   }
+//   render() {
+//     this.el.classList.add("search");
+//     this.el.innerHTML = /*html*/ `
+//     <input />
+//     <button>Click</button>
+//     `;
+
+//     const inputEl = this.el.querySelector("input");
+//     inputEl.oninput = () => {
+//       this.state.inputText = inputEl.value;
+//     };
+
+//     const buttonEl = this.el.querySelector("button");
+//     buttonEl.addEventListener("click", () => {
+//       console.log(this.state.inputText );
+//     });
+//   }
+// }
